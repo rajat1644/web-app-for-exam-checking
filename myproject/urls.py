72a01bd4a5234cc9d8098c9ec960bb85f55b502c
RAJATBIR SINGH WALIA <rajatbirsinghwalia@gmail.com>
2025-07-11 21:40:30 +0530

    Public landing page for preview2

--- myproject/urls.py
+++ myproject/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', public_demo),  # 👈 Make this the default page
 ]
 
-'''
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('internal/', include('tasks.urls')),
@@ -53,6 +53,6 @@ urlpatterns = [
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='registration/password_change_done.html'), name='password_change_done'),
 ]
-'''
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
